6887909b25bdd8ea0dbc2bf77b98f6b3bc4c4c2c
AbdulRehmani2 <abdulrehmani2238@gmail.com>
2024-07-24 11:57:24 +0500

    Added Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Navbar from "./components/Navbar"
+import Home from "./routes/Home"
+import Coins from "./routes/Coins"
+import Account from "./routes/Account"
+import NotFound from "./routes/NotFound"
 
 function App() {
   return (
-    <>
-      <h1 className="text-3xl text-purple-950 text-center font-bold">Every thing working.</h1>
-    </>
+    <div>
+      <BrowserRouter>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<Home/>}></Route>
+          <Route path="/coins:id" element={<Coins/>}></Route>
+          <Route path="/account" element={<Account/>}></Route>
+          <Route path="/*" element={<NotFound/>}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 

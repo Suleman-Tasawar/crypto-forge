5062dda614b95b95003cedad7e2a441a02325f22
Suleman Tasawar <sulemantasawar9@gmail.com>
2024-07-27 16:57:54 +0500

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/NavBar.jsx";
 import Home from "./routes/Home";
 import CoinsDetail from "./routes/CoinsDetail";
 import Account from "./routes/Account";
--- src/components/TrendingNews.jsx
+++ src/components/TrendingNews.jsx
@@ -12,7 +12,7 @@ const TrendingNews = ({newsLink,newsImg,newsTitle,newsText,newsAuthorName,newsNa
       <div className='w-[70%] p-5'>
         <h2 className='mt-5 text-xl font-bold'>{newsTitle}</h2>
         <p className='mt-5 text-base'>{newsText}</p>
-        <a className='mt-5 hover:text-gray-800 hover:underline hover:font-bold hover:transition-all' href={newsLink}>Visit Site</a>
+        <a className='mt-5 hover:text-gray-100 text-gray-200 hover:underline hover:font-bold hover:transition-all' href={newsLink}>Visit Site</a>
         <div className='mt-5'>
           <span className='mt-5 w-5 h-3 bg-gray-800 rounded-sm text-white p-1'>{newsAuthorName == null ? "Unknown" : newsAuthorName}</span>
           <span className='mt-5 ml-2 font-semibold'>{newsName}</span>

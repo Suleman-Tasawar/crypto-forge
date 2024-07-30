f36c2f37e57e66fee2ab2a587f8dd436eeb3ab98
Suleman Tasawar <sulemantasawar9@gmail.com>
2024-07-30 19:03:48 +0500

    fixed the api issue

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      "/api": "https://api.coingecko.com"
-    }
-  },
+  // server: {
+  //   proxy: {
+  //     "/api": "https://api.coingecko.com"
+  //   }
+  // },
   plugins: [react()],
 })
